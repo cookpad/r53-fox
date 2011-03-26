0a687b034edc45e8a56bf91e93e07952445512ec
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-26 11:43:18 +0900

    make skeleton

---
+++ Resources/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+pref("toolkit.defaultChromeURI", "chrome://r53fox/content/main.xul");
+pref("toolkit.defaultChromeFeatures", "chrome,centerscreen,width=800,height=600,resizable");
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
