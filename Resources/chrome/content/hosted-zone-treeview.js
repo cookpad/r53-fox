22e0dd118e7ca108aa9bc3523f8e456da9bad973
winebarrel <sgwr_dts@yahoo.co.jp>
2011-03-27 22:59:06 +0900

    etc,etc,etc...

--- Resources/chrome/content/hosted-zone-treeview.js
+++ Resources/chrome/content/hosted-zone-treeview.js
@@ -79,35 +79,6 @@ HostedZoneTreeView.prototype = {
     }.bind(this), $('main-window-loader'));
 
     this.invalidate();
-    /*
-    this.rows.length = 0;
-
-    protect(function() {
-      var xhr = inProgress(function() {
-        return this.iamcli.query_or_die('ListUsers');
-      }.bind(this));
-
-      var pathList = ['/'];
-
-      for each (var member in xhr.xml()..Users.member) {
-        this.rows.push(member);
-        pathList.push(member.Path.toString());
-      }
-
-      var pathFilter = $('user-tree-path-filter');
-      pathFilter.removeAllItems();
-      pathList = pathList.uniq().sort();
-
-      for (var i = 0; i < pathList.length; i++) {
-        var path = pathList[i];
-        pathFilter.appendItem(path, path);
-      }
-
-      pathFilter.selectedIndex = 0;
-
-      this.invalidate();
-    }.bind(this));
-      */
   },
 
   selectedRow: function() {
@@ -135,82 +106,23 @@ HostedZoneTreeView.prototype = {
   },
   
 
-  deleteRow: function() {
+  deleteHostedZone: function() {
     var row = this.selectedRow();
     if (!row) { return; }
 
+    var hzid = this.hostedZoneId(row);
     var name = row.Name.toString();
 
     if (!confirm("Are you sure you want to delete '" + name + "' ?")) {
       return;
     }
 
-    /*
-    protect(function() {
-      inProgress(function() {
-        var xhr = this.iamcli.query_or_die('ListAccessKeys', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..AccessKeyMetadata.member) {
-          var params = [['UserName', userName], ['AccessKeyId', member.AccessKeyId]];
-          this.iamcli.query_or_die('DeleteAccessKey', params);
-        }
-
-        xhr = this.iamcli.query_or_die('ListGroupsForUser', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..Groups.member) {
-          var params = [['UserName', userName], ['GroupName', member.GroupName]];
-          this.iamcli.query_or_die('RemoveUserFromGroup', params);
-        }
-
-        xhr = this.iamcli.query_or_die('ListSigningCertificates', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..Certificates.member) {
-          var params = [['UserName', userName], ['CertificateId', member.CertificateId]];
-          this.iamcli.query_or_die('DeleteSigningCertificate', params);
-        }
-
-        xhr = this.iamcli.query_or_die('ListUserPolicies', [['UserName', userName]]);
-
-        for each (var member in xhr.xml()..PolicyNames.member) {
-          var params = [['UserName', userName], ['PolicyName', member]];
-          this.iamcli.query_or_die('DeleteUserPolicy', params);
-        }
-
-        this.iamcli.query_or_die('DeleteUser', [['UserName', userName]]);
-
-        Prefs.deleteUserAccessKeyId(userName);
-        Prefs.deleteUserSecretAccessKey(userName);
-
-        this.refresh();
-      }.bind(this));
-    }.bind(this));
-      */
+    $R53(function(r53cli) {
+      r53cli.deleteHostedZone('hzid');
+      this.refresh();
+    }.bind(this), $('main-window-loader'));
   },
 
-  /*
-  openUserEditDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-edit-dialog.xul', 'user-edit-dialog', 'chrome,modal',
-               {view:this, inProgress:inProgress, user:user});
-  },
-
-  openViewKeyDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-view-key-dialog.xul', 'user-edit-dialog', 'chrome,modal',
-               {view:this, inProgress:inProgress, user:user});
-  },
-
-  openUserCreateLoginProfileDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-create-login-profile-dialog.xul', 'user-create-login-profile-dialog', 'chrome,modal',
-               {view:this, inProgress:inProgress, user:user});
-  },
-
-  openUserOpenConsoleDialog: function() {
-    var user = this.selectedRow();
-    openDialog('chrome://iamfox/content/user-open-console-dialog.xul', 'user-open-console-dialog', 'chrome,modal', {user:user});
-  },
-*/
   selectByName: function(name) {
     for (var i = 0; i < this.rows.length; i++) {
       var row = this.rows[i];
@@ -222,39 +134,12 @@ HostedZoneTreeView.prototype = {
 
     this.rows
   },
-/*
-  openUserGroupWindow: function() {
-    var user = this.selectedRow();
 
-    openModalWindow('user-group-window.xul', 'user-cert-window', 400, 400,
-                    {iamcli:this.iamcli, userName:user.UserName});
-  },
-
-  deleteLoginProfile: function() {
-    var user = this.selectedRow();
-    var userName = user.UserName;
-
-    if (!confirm("Are you sure you want to delete '" + userName + "'s login profile' ?")) {
-      return;
-    }
-
-    protect(function() {
-      inProgress(function() {
-        this.iamcli.query_or_die('DeleteLoginProfile', [['UserName', userName]]);
-      }.bind(this));
-    }.bind(this));
-  },
-
-    */
   copyColumnToClipboard: function(name) {
-    try {
     var row = this.selectedRow();
 
     if (row) {
       copyToClipboard($CELL(row, name, 0));
-    }
-    }catch (e) {
-      alert(e);
     }
   },
 
