4008d9d660a2e14540137acd419b928aba1b6d03
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-03-28 04:12:21 +0900

    etc

--- pack-osx.sh
+++ pack-osx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-APP=IAMFox.app
+APP=R53Fox.app
 
-rm -rf IAMFox.app *.dmg
-cp -pr IAMFox-src.app IAMFox.app
-cp -p /Library/Frameworks/XUL.framework/Versions/Current/xulrunner IAMFox.app/Contents/MacOS
-mkdir IAMFox.app/Contents/Frameworks
-rsync -rl /Library/Frameworks/XUL.framework IAMFox.app/Contents/Frameworks/
+rm -rf R53Fox.app *.dmg
+cp -pr R53Fox-src.app R53Fox.app
+cp -p /Library/Frameworks/XUL.framework/Versions/Current/xulrunner R53Fox.app/Contents/MacOS
+mkdir R53Fox.app/Contents/Frameworks
+rsync -rl /Library/Frameworks/XUL.framework R53Fox.app/Contents/Frameworks/
