458d327f7cc5d2a3d87a6c99b61680c036fd7ea0
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-02 00:24:35 +0900

    add default.js to xpi

--- pack-xpi.sh
+++ pack-xpi.sh
@@ -4,8 +4,10 @@ XPI=r53-fox-${VERSION}.xpi
 
 rm -rf *.xpi 
 cp -r Resources/chrome ./xpi
+mkdir -p xpi/defaults/preferences/
+cp Resources/defaults/preferences/default.js xpi/defaults/preferences/
 cd xpi/
 zip -r $XPI .
 mv $XPI ../
 cd ../
-rm -rf ./xpi
+rm -rf xpi
