19cc65a003e14a55cb67a04ee46fc87320063390
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-02 01:15:33 +0900

    etc

--- pack-xpi.sh
+++ pack-xpi.sh
@@ -8,6 +8,7 @@ mkdir -p xpi/defaults/preferences/
 cp Resources/defaults/preferences/default.js xpi/defaults/preferences/
 cd xpi/
 echo '.exclude-in-xpi { display:none; }' >> skin/classic/r53fox.css
+sed -i 's|<window|\0 sizemode="maximized"|' content/main.xul
 zip -r $XPI .
 mv $XPI ../
 cd ../
