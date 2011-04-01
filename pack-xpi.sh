2d3feef5123cb594d11b309d882e49cafe0bbefe
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-02 00:49:42 +0900

    add .exclude-in-xpi class

--- pack-xpi.sh
+++ pack-xpi.sh
@@ -7,6 +7,7 @@ cp -r Resources/chrome ./xpi
 mkdir -p xpi/defaults/preferences/
 cp Resources/defaults/preferences/default.js xpi/defaults/preferences/
 cd xpi/
+echo '.exclude-in-xpi { display:none; }' >> skin/classic/r53fox.css
 zip -r $XPI .
 mv $XPI ../
 cd ../
