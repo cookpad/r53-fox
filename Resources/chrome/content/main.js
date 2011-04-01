0c3bf0534688ebedc385711e762ad3713c092ffc
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-02 00:44:38 +0900

    etc

--- Resources/chrome/content/main.js
+++ Resources/chrome/content/main.js
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 Components.utils.import('resource://r53fox/preferences.jsm');
+
 function onLoad() {
   var tree = $('hosted-zone-tree');
   $view = new HostedZoneTreeView();
