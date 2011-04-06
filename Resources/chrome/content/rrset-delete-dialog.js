6f1fef415bb364f18849cb3cdced7656dbfe44d9
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-07 00:57:48 +0900

    change window title

--- Resources/chrome/content/rrset-delete-dialog.js
+++ Resources/chrome/content/rrset-delete-dialog.js
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 function onLoad() {
   var name = window.arguments[0].name;
-  document.title = "Delete '" + name + "'";
+  document.title = "Delete - " + name;
 }
 
 function onDialogAccept() {
