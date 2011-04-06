833ea74061cfacd10c784b1d7f9f5c6c5ccbf984
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-07 02:32:11 +0900

    no varbose

--- pack-xpi.sh
+++ pack-xpi.sh
@@ -9,7 +9,7 @@ cp Resources/defaults/preferences/default.js xpi/defaults/preferences/
 cd xpi/
 echo '.exclude-in-xpi { display:none; }' >> skin/classic/r53fox.css
 sed -i 's|<window|\0 sizemode="maximized"|' content/main.xul
-zip -r $XPI .
+zip -r $XPI . > /dev/null
 mv $XPI ../
 cd ../
 rm -rf xpi
