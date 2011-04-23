f34bc004b8266264afeb9ae94e209a8d0c5bd60e
winebarrel <sgwr_dts@yahoo.co.jp>
2011-04-24 00:59:14 +0900

    add new props

--- Resources/defaults/preferences/default.js
+++ Resources/defaults/preferences/default.js
@@ -1,3 +1,6 @@
+pref('r53fox.version', '');
+pref('r53fox.currentUser', '');
+pref('r53fox.accounts', '');
 pref('r53fox.accessKeyId', '');
 pref('r53fox.secretAccessKey', '');
 pref('r53fox.changeIds', '({})');
