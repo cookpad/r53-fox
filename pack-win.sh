2996809ea77620d0f7473b55a16125dbb5879765
winebarrel <sgwr_dts@yahoo.co.jp>
2011-05-11 22:03:23 +0900

    change exe filename

--- pack-win.sh
+++ pack-win.sh
@@ -6,7 +6,7 @@ rm -rf $SRC_DIR *setup.exe
 cp -pr Resources $SRC_DIR
 cd $SRC_DIR
 cp chrome/icons/default/main-window.ico r53-fox.ico
-cp /usr/local/xulrunner/xulrunner-stub.exe ./r53-fox.exe
+cp /usr/local/xulrunner/xulrunner-stub.exe ./
 rsync -rl /usr/local/xulrunner ./
 cd ..
 cygstart -w r53-fox.ci
